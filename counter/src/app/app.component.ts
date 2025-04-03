f3141563127c10c1a8917dd4c7ef7f366640eda5
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-03 18:07:43 +0700

    new commit

--- counter/src/app/app.component.ts
+++ counter/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CounterComponent } from "./counter/counter.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [CounterComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'counter';
+  title = "counter";
 }
