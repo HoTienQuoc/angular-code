59420d0422eccc875ffacc614fa35dacc95cca74
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-04 12:00:25 +0700

    new commit

--- simple-todo/src/app/app.component.ts
+++ simple-todo/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { TodoListComponent } from "./todo-list/todo-list.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [TodoListComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'simple-todo';
+  title = "simple-todo";
 }
