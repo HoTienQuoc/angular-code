d6d2550f33873d023c39359d14a12a78f4d0a1c9
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-05 20:59:21 +0700

    new commit

--- guess-the-number/src/app/app.component.ts
+++ guess-the-number/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { GuessTheNumberComponent } from "./guess-the-number/guess-the-number.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [GuessTheNumberComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'guess-the-number';
+  title = "guess-the-number";
 }
