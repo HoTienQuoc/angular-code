7644ee91db45eec78b13b53e6bc80aa0f86282ad
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-05 21:09:46 +0700

    new commit

--- guess-the-number/src/app/guess-the-number/guess-the-number.component.ts
+++ guess-the-number/src/app/guess-the-number/guess-the-number.component.ts
@@ -9,7 +9,23 @@ import { FormsModule } from "@angular/forms";
   styleUrl: "./guess-the-number.component.scss",
 })
 export class GuessTheNumberComponent {
+  secretNumber = this.generateRandomNumber();
+  attemptsLeft = 10;
+  guessedNumber?: number;
+  feedbackMessage = "";
+  gameOver = false;
+  private static readonly MAX_NUMBER = 100;
+  private static readonly MAX_ATTEMPTS = 10;
+
   private generateRandomNumber(): number {
     return Math.floor(Math.random() * 100) + 1;
   }
+  public isValidGuess(guess?: number): boolean {
+    return (
+      guess !== undefined &&
+      guess >= 1 &&
+      guess <= GuessTheNumberComponent.MAX_NUMBER
+    );
+  }
+  
 }
