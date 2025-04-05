7362c80b9a2781ca47e67b9d3c5ab391a177893d
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-05 20:05:35 +0700

    new commit

--- stopwatch/src/app/stopwatch/stopwatch.component.ts
+++ stopwatch/src/app/stopwatch/stopwatch.component.ts
@@ -7,4 +7,29 @@ import { Component } from "@angular/core";
   templateUrl: "./stopwatch.component.html",
   styleUrl: "./stopwatch.component.scss",
 })
-export class StopwatchComponent {}
+export class StopwatchComponent {
+  elapsedTime = 0;
+  isRunning = false;
+  intervalRef: any;
+  startStop() {
+    this.isRunning ? this.Stop() : this.Start();
+  }
+  private Start() {
+    this.isRunning = true;
+    this.intervalRef = setInterval(() => {
+      this.elapsedTime += 0.1;
+    }, 100);
+    console.log("StopWatch started");
+  }
+  private Stop() {
+    this.isRunning = false;
+    clearInterval(this.intervalRef);
+    console.log("StopWatch stopped");
+  }
+  private Reset() {
+    this.isRunning = false;
+    clearInterval(this.intervalRef);
+    this.elapsedTime = 0;
+    console.log("StopWatch stopped");
+  }
+}
