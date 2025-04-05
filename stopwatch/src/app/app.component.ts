e5959fcd98a2c157917bfaff73d593b8ed2fef21
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-05 19:52:26 +0700

    hh

--- stopwatch/src/app/app.component.ts
+++ stopwatch/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
+import { StopwatchComponent } from "./stopwatch/stopwatch.component";
 
 @Component({
   selector: "app-root",
-  imports: [],
+  imports: [StopwatchComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
---
+++ stopwatch/src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-stopwatch",
+  imports: [CommonModule],
+  templateUrl: "./stopwatch.component.html",
+  styleUrl: "./stopwatch.component.scss",
+})
+export class StopwatchComponent {}
