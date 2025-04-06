bfc105ec839f0aa62a803ed022aa8926f7ef4306
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-07 00:29:27 +0700

    new commit

--- paper-rock-scissors/src/app/game/game.component.ts
+++ paper-rock-scissors/src/app/game/game.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-game',
-  imports: [],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  selector: "app-game",
+  imports: [CommonModule],
+  templateUrl: "./game.component.html",
+  styleUrl: "./game.component.scss",
 })
 export class GameComponent {
+  choices = ["rock", "paper", "scissors"];
+  playerChoice: string | null = null;
+  computerChoice: string | null = null;
+  result: string | null = null;
 
+  getRandomNumber(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
+  determineWinner(playerChoice: string, computerChoice: string): string {
+    if (playerChoice === computerChoice) {
+      return "It's a tie!";
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      return "You win!";
+    } else {
+      return "Computer wins!";
+    }
+  }
+  play(choice: string): void {
+    this.playerChoice = choice;
+    this.computerChoice =
+      this.choices[this.getRandomNumber(this.choices.length)];
+    this.result = this.determineWinner(choice, this.computerChoice);
+  }
 }
