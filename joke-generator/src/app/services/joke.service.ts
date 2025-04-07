7ff9ad14c49a0742ffa8ec0b53ccac708b3b3b5a
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-07 14:45:04 +0700

    Update joke.service.ts

--- joke-generator/src/app/services/joke.service.ts
+++ joke-generator/src/app/services/joke.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class JokeService {
+  private apiUrl = "https://official-joke-api.appspot.com/jokes/random";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getRandomJoke(): Observable<{ setup: string; punchline: string }> {
+    return this.http.get<{ setup: string; punchline: string }>(this.apiUrl);
+  }
 }
