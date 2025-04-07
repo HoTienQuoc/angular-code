b4721bfe904668a49eafdc2c2517ef464438691f
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-07 14:33:30 +0700

    new commit

--- joke-generator/src/app/app.component.ts
+++ joke-generator/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { JokeComponent } from "./joke/joke.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [JokeComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'joke-generator';
+  title = "joke-generator";
 }
