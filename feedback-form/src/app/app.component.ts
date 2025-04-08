1287299d42b99437350408eb6d81f240225713f5
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-08 12:28:49 +0700

    new commit

--- feedback-form/src/app/app.component.ts
+++ feedback-form/src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'feedback-form';
-}
+export class AppComponent {}
