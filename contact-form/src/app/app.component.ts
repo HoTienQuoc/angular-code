1eeeec567b0d5cf56e606f6cd4f706e73d0de1cd
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-08 18:11:28 +0700

    new commit

--- contact-form/src/app/app.component.ts
+++ contact-form/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
 
 @Component({
   selector: "app-root",
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
