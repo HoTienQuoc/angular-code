5c74e65414bbdf59a19f719f117d9682913bf375
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-10 20:40:10 +0700

    new commit

--- custom-directives/src/app/directives/hover-lighlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HoverLighlightDirective } from './hover-lighlight.directive';
-
-describe('HoverLighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HoverLighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
