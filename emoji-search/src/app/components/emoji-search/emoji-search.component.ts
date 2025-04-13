41d343d409beec7a7d3f99784dd5391ddca3fc70
HoTienQuoc <hotienquoc0429@gmail.com>
2025-04-13 13:21:25 +0700

    new commit

---
+++ emoji-search/src/app/components/emoji-search/emoji-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emoji-search',
+  imports: [],
+  templateUrl: './emoji-search.component.html',
+  styleUrl: './emoji-search.component.scss'
+})
+export class EmojiSearchComponent {
+
+}
